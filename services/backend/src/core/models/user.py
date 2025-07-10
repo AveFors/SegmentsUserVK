557fab49c31ee9ea7d2f5c46db05d7d4fe0135ca
Maksim Kharitonov <haritonovmaksimka2007@mail.ru>
2025-07-10 14:42:14 +0300

    Added models

---
+++ services/backend/src/core/models/user.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.models.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    external_id: Mapped[int] = mapped_column(unique=True, index=True)
+    segments: Mapped[list["UserSegment"]] = relationship(back_populates="user", cascade="all, delete-orphan")
